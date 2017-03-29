1491e659e050aed12a7922e8689503166ad742e4
Leonardo Alves de Melo <leonardoalvesdemelo@Leonardos-MacBook-Pro.local>
2017-03-29 11:23:56 -0300

    Projeto master criado

---
+++ WhatsTheBest/WhatsTheBest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatsTheBest
+//
+//  Created by Leonardo Alves de Melo on 3/29/17.
+//  Copyright © 2017 Leonardo Alves de Melo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
