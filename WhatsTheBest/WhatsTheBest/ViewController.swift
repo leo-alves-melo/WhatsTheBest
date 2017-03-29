c4326f776c82ae5cd183a7deeb41f6bd55265c17
Leonardo Alves de Melo <leonardoalvesdemelo@Leonardos-MacBook-Pro.local>
2017-03-29 12:05:36 -0300

    Criada primeira interface ainda não funcional da View da estrela

--- WhatsTheBest/WhatsTheBest/ViewController.swift
+++ WhatsTheBest/WhatsTheBest/ViewController.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chooserButton: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        chooserButton.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "moveChooser:"))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func moveChooser(pan: UIPanGestureRecognizer) {
+        
     }
 
 
