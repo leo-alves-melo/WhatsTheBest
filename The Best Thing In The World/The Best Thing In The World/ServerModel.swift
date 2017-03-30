70910c5ac2e473ee5cd473458603836533805aad
Leonardo Alves de Melo <leonardoalvesdemelo@Leonardos-MacBook-Pro.local>
2017-03-30 10:05:52 -0300

    Criada classe de serviço pro servidor e o model do servidor

---
+++ The Best Thing In The World/The Best Thing In The World/ServerModel.swift
@@ -0,0 +1,16 @@
+//
+//  ServerModel.swift
+//  The Best Thing In The World
+//
+//  Created by Leonardo Alves de Melo on 3/30/17.
+//  Copyright © 2017 Gustavo De Mello Crivelli. All rights reserved.
+//
+
+import Foundation
+
+enum RankingType:Int {
+    case allTime = 0
+    case lastMonth = 1
+    case lastWeek = 2
+    case today = 3
+}
---
+++ The Best Thing In The World/The Best Thing In The World/ServerService.swift
@@ -0,0 +1,38 @@
+//
+//  ServerService.swift
+//  The Best Thing In The World
+//
+//  Created by Leonardo Alves de Melo on 3/30/17.
+//  Copyright © 2017 Gustavo De Mello Crivelli. All rights reserved.
+//
+
+import Foundation
+
+class ServerService {
+    
+    private let urlServer = "http://www.meuserver.com/"
+    private var user:User? = nil
+    
+    func getRandomItem() -> Item? {
+        return nil
+    }
+    
+    func getRanking(type:RankingType) -> [Item]? {
+        switch type {
+        case .allTime:
+            print("all time")
+        case .lastMonth:
+            print("last month")
+        case .lastWeek:
+            print("last week")
+        case .today:
+            print("today")
+            
+        }
+        return nil
+    }
+    
+    func uploadNewItemToServer(item:Item?) -> Bool {
+        return false
+    }
+}
