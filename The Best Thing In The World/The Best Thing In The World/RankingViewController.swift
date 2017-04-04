f434b1349d8c543ad07aa67f27fd0c34d058310f
Jessica Batista de Barros Cherque <jessica.jbbarros@gmail.com>
2017-04-04 10:21:08 -0300

    Mostrando o ranking vindo do json

--- The Best Thing In The World/The Best Thing In The World/RankingViewController.swift
+++ The Best Thing In The World/The Best Thing In The World/RankingViewController.swift
@@ -19,24 +19,14 @@ class RankingViewController: UIViewController, UITableViewDataSource , UITableVi
         super.viewDidLoad()
         self.tabBar.selectedItem = self.tabBar.items![0]
         
-//        listItems = ServerService().getRanking(type: RankingType.allTime.rawValue)!
-        
-        let user = User(id: 1234, name: "José", gender: "f", age: 19, profile: "bla bla bla", score: 300)
-        let item = Item(id: 1001, subtitle: "Star", imageLink: "star", score: 2398402, owner: user, date: "19/08/2016")
-
-        listItems = [item, item, item, item]
+        listItems = ServerService().getRanking(type: RankingType.allTime.rawValue)!
 
     }
     
     func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
         let tag = tabBar.selectedItem!.tag
         
-//        listItems = ServerService().getRanking(type: tag)!
-        
-        let user = User(id: 1234, name: "Ana", gender: "f", age: 19, profile: "bla bla bla", score: 566)
-        let item = Item(id: 1001, subtitle: "Estrela", imageLink: "star", score: 10000, owner: user, date: "19/08/2016")
-        
-        listItems = [item, item, item, item]
+        listItems = ServerService().getRanking(type: tag)!
         
         tableView.reloadData()
     }
